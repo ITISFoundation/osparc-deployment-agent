7e21edaab33b7a18b2bb4361aa913c496702e9d1
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-12-22 14:48:50 +0100

    fix cmd line tests

--- src/simcore_service_deployment_agent/cmd_utils.py
+++ src/simcore_service_deployment_agent/cmd_utils.py
@@ -10,8 +10,8 @@ async def run_cmd_line(cmd: str) -> str:
     proc = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
+
     stdout, stderr = await proc.communicate()
-    await proc.wait()
     log.debug("[{%s}] exited with %s]", cmd, proc.returncode)
 
     if proc.returncode > 0:
--- tests/unit/test_cmd_utils.py
+++ tests/unit/test_cmd_utils.py
@@ -9,13 +9,10 @@ import pytest
 from simcore_service_deployment_agent import cmd_utils, exceptions
 
 
-async def test_valid_cmd():
-    try:
-        await cmd_utils.run_cmd_line("whoami")
-    except exceptions.CmdLineError:
-        pytest.fail("Unexpected error")
+async def test_valid_cmd(loop):
+    await cmd_utils.run_cmd_line("whoami")
 
 
-async def test_invalid_cmd():
+async def test_invalid_cmd(loop):
     with pytest.raises(exceptions.CmdLineError):
         await cmd_utils.run_cmd_line("whoamiasd")
