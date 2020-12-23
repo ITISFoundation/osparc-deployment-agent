d1ba7fbbe7f6ddfd112a236d73c97a9fb73631a6
sanderegg <35365065+sanderegg@users.noreply.github.com>
2020-12-23 15:42:46 +0100

    added mocks for mattermost

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -44,11 +44,41 @@ def portainer_stacks(valid_config: Dict[str, Any]) -> Dict[str, Any]:
 
 
 @pytest.fixture()
-async def portainer_service_mock(
-    bearer_code: str, portainer_stacks: Dict[str, Any], valid_config: Dict[str, Any]
-) -> aioresponses:
+def aioresponse_mocker() -> aioresponses:
     PASSTHROUGH_REQUESTS_PREFIXES = ["http://127.0.0.1", "ws://"]
+    with aioresponses(passthrough=PASSTHROUGH_REQUESTS_PREFIXES) as mock:
+        yield mock
 
+
+@pytest.fixture()
+async def mattermost_service_mock(
+    aioresponse_mocker: aioresponses, valid_config: Dict[str, Any]
+) -> aioresponses:
+    get_channels_pattern = re.compile(
+        rf'{valid_config["main"]["notifications"][0]["url"]}/api/v4/channels/.+'
+    )
+    aioresponse_mocker.get(
+        get_channels_pattern, status=200, payload={"header": "some text in the header"}
+    )
+    aioresponse_mocker.put(
+        get_channels_pattern, status=200, payload={"success": "bravo"}
+    )
+    aioresponse_mocker.post(
+        f'{valid_config["main"]["notifications"][0]["url"]}/api/v4/posts',
+        status=201,
+        payload={"success": "bravo"},
+    )
+
+    yield aioresponse_mocker
+
+
+@pytest.fixture()
+async def portainer_service_mock(
+    aioresponse_mocker: aioresponses,
+    bearer_code: str,
+    portainer_stacks: Dict[str, Any],
+    valid_config: Dict[str, Any],
+) -> aioresponses:
     def _check_auth(**kwargs) -> bool:
         return (
             "headers" in kwargs
@@ -113,21 +143,24 @@ async def portainer_service_mock(
     create_stack_pattern = re.compile(r"http://[a-z\-0-9_]+:[0-9]+/api/stacks")
     update_stack_pattern = re.compile(r"http://[a-z\-0-9_]+:[0-9]+/api/stacks/[0-9]+")
 
-    with aioresponses(passthrough=PASSTHROUGH_REQUESTS_PREFIXES) as mock:
-        mock.post(
-            post_authenticate_pattern,
-            status=200,
-            payload={"jwt": bearer_code},
-            repeat=True,
-        )
-        mock.get(get_swarm_id_pattern, callback=get_docker_swarm_cb, repeat=True)
-        mock.get(get_endpoints_pattern, callback=get_endpoints_cb, repeat=True)
+    aioresponse_mocker.post(
+        post_authenticate_pattern,
+        status=200,
+        payload={"jwt": bearer_code},
+        repeat=True,
+    )
+    aioresponse_mocker.get(
+        get_swarm_id_pattern, callback=get_docker_swarm_cb, repeat=True
+    )
+    aioresponse_mocker.get(
+        get_endpoints_pattern, callback=get_endpoints_cb, repeat=True
+    )
 
-        mock.put(update_stack_pattern, status=200, repeat=True)
-        mock.post(create_stack_pattern, callback=create_stack_cb, repeat=True)
-        mock.get(get_stacks_pattern, callback=get_stacks_cb, repeat=True)
+    aioresponse_mocker.put(update_stack_pattern, status=200, repeat=True)
+    aioresponse_mocker.post(create_stack_pattern, callback=create_stack_cb, repeat=True)
+    aioresponse_mocker.get(get_stacks_pattern, callback=get_stacks_cb, repeat=True)
 
-        yield mock
+    yield aioresponse_mocker
 
 
 @pytest.fixture()
