1876262a5a41fdea6b0a472a9baf7bc8595360b7
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-02-17 22:11:44 +0100

    moved portainer fixture to fixtures folder

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,10 @@ import simcore_service_deployment_agent
 ## HELPERS
 current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
+
+## FIXTURES
+pytest_plugins = ["fixtures.fixture_portainer"]
+
 ## DIRs
 
 
