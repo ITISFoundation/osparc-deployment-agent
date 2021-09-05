6f5566106a1663a6cec0d3310eff418ade13e6e5
sanderegg <35365065+sanderegg@users.noreply.github.com>
2021-09-05 22:50:43 +0200

    reqs with python 3.8.10

--- setup.py
+++ setup.py
@@ -7,42 +7,42 @@ from setuptools import find_packages, setup
 here = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
 
 
-def read_reqs( reqs_path: Path):
-    return re.findall(r'(^[^#-][\w]+[-~>=<.\w]+)', reqs_path.read_text(), re.MULTILINE)
+def read_reqs(reqs_path: Path):
+    return re.findall(r"(^[^#-][\w]+[-~>=<.\w]+)", reqs_path.read_text(), re.MULTILINE)
 
 
-#-----------------------------------------------------------------
+# -----------------------------------------------------------------
 
-install_requirements = read_reqs( here / "requirements" / "_base.txt" ) + [
-    "simcore-service-library==0.1.0",
+install_requirements = read_reqs(here / "requirements" / "_base.txt") + [
+    "simcore-service-library",
 ]
-test_requirements = read_reqs( here / "requirements" / "_test.txt" )
+test_requirements = read_reqs(here / "requirements" / "_test.txt")
 
 setup(
-    name='simcore-service-deployment-agent',
-    version='0.9.3',
-    description='Agent that automatically deploy services in a swarm',
-    packages=find_packages(where='src'),
+    name="simcore-service-deployment-agent",
+    version="0.9.3",
+    description="Agent that automatically deploy services in a swarm",
+    packages=find_packages(where="src"),
     package_dir={
-        '': 'src',
+        "": "src",
     },
     include_package_data=True,
     package_data={
-        '': [
-            'config/*.y*ml',
-            'oas3/v0/*.y*ml',
-            'oas3/v0/components/schemas/*.y*ml',
-            'data/*.json',
-            'templates/**/*.html',
-            ]
-    },
-    entry_points={
-        'console_scripts': [
-            'simcore-service-deployment-agent = simcore_service_deployment_agent.cli:main',
+        "": [
+            "config/*.y*ml",
+            "oas3/v0/*.y*ml",
+            "oas3/v0/components/schemas/*.y*ml",
+            "data/*.json",
+            "templates/**/*.html",
         ]
     },
-    python_requires='>=3.6',
+    entry_points={
+        "console_scripts": [
+            "simcore-service-deployment-agent = simcore_service_deployment_agent.cli:main",
+        ]
+    },
+    python_requires=">=3.6",
     install_requires=install_requirements,
     tests_require=test_requirements,
-    setup_requires=['pytest-runner']
+    setup_requires=["pytest-runner"],
 )
