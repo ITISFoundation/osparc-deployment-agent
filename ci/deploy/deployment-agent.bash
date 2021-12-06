7ab1e90c42432404b2418b120945f44daf19d396
Dustin Kaiser <Dustin.Kaiser@gmx.net>
2021-12-06 15:03:45 +0100

    Update ci/deploy/deployment-agent.bash
    Co-authored-by: Sylvain <35365065+sanderegg@users.noreply.github.com>

--- ci/deploy/deployment-agent.bash
+++ ci/deploy/deployment-agent.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
-# as of http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -euo pipefail
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
 IFS=$'\n\t'
 
 bash ci/helpers/dockerhub_login.bash
