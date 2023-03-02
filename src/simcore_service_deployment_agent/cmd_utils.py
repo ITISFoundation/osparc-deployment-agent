6f2bd540e7d91357e95c847c1b8d21ad74dfa808
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-03-02 11:53:46 +0100

    @sanderegg review: refactor cmd_utils

--- src/simcore_service_deployment_agent/cmd_utils.py
+++ src/simcore_service_deployment_agent/cmd_utils.py
@@ -1,10 +1,34 @@
 import asyncio
 import logging
-from typing import Optional
+from asyncio.subprocess import Process
+from typing import Optional, Union
 
 from .exceptions import CmdLineError
 
 log = logging.getLogger(__name__)
+
+
+async def _wait_and_process_results(
+    process: Process, *, command: Union[str, list[str]], strip_endline: bool = True
+) -> Optional[str]:
+    # waits
+    stdout, stderr = await process.communicate()
+    log.debug("[{%s}] exited with %s]", command, process.returncode)
+
+    # process results
+    if process.returncode > 0:
+        error_data = ""
+        if stderr:
+            error_data = stderr.decode()
+            log.debug("\n[stderr]%s", error_data)
+        raise CmdLineError(command, error_data)
+
+    if stdout:
+        standard_output = stdout.decode()
+        log.debug("\n[stdout]%s", standard_output)
+
+        return standard_output.strip("\n") if strip_endline else standard_output
+    return None
 
 
 async def run_cmd_line(
@@ -27,25 +51,19 @@ async def run_cmd_line(
             " ".join(cmd), "The command was invalid and the cmd call failed."
         ) from e
 
-    stdout, stderr = await proc.communicate()
-    log.debug("[{%s}] exited with %s]", cmd, proc.returncode)
-
-    if proc.returncode > 0:
-        error_data = ""
-        if stderr:
-            error_data = stderr.decode()
-            log.debug("\n[stderr]%s", error_data)
-        raise CmdLineError(cmd, error_data)
-
-    if stdout:
-        standard_output = stdout.decode()
-        log.debug("\n[stdout]%s", standard_output)
-
-        return standard_output.strip("\n") if strip_endline else standard_output
-    return None
+    return await _wait_and_process_results(
+        proc, command=cmd, strip_endline=strip_endline
+    )
 
 
-async def run_cmd_line_unsafe(cmd: str, cwd_: str = ".") -> str:
+async def run_cmd_line_unsafe(
+    cmd: str, cwd_: str = ".", *, strip_endline: bool = True
+) -> Optional[str]:
+    """
+
+    returns output.strip('\n') or None if no outputs
+    raises CmdLineError
+    """
     proc = await asyncio.create_subprocess_shell(
         cmd,
         stdout=asyncio.subprocess.PIPE,
@@ -53,18 +71,6 @@ async def run_cmd_line_unsafe(cmd: str, cwd_: str = ".") -> str:
         cwd=cwd_,
     )
 
-    stdout, stderr = await proc.communicate()
-    log.debug("[{%s}] exited with %s]", cmd, proc.returncode)
-
-    if proc.returncode > 0:
-        error_data = ""
-        if stderr:
-            error_data = stderr.decode()
-            log.debug("\n[stderr]%s", error_data)
-        raise CmdLineError(cmd, error_data)
-
-    if stdout:
-        data = stdout.decode()
-        log.debug("\n[stdout]%s", data)
-        return data
-    return _EMPTY_STRING
+    return await _wait_and_process_results(
+        proc, command=cmd, strip_endline=strip_endline
+    )
